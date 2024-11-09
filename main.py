ce83ac5d3d00efb0773b16a599be2cba37b0d95b
Aram <89261203+Mystic-altf4@users.noreply.github.com>
2024-11-09 14:35:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import random
-ilk_grup = ["Mr.","Kylian","ps5","Why","Eti","DA DA DA DA","Adolf","Q7","Skibidi","Arctic"]
-ikinci_grup = ["Whopper", "Beast", "Offside","So","d","Cin","MAX","Error","Red","Kraga"]
-son_grup = ["404","McDonalds","McCrappe","Werner","Serious","Trump","VERSTAPPEN","Stalin","Bottler","505"]
+ilk_grup = ["Mr.","Kylian","ps5","Why","Eti","DA DA DA DA","Adolf","Q7","Skibidi","Arctic","Have"]
+ikinci_grup = ["Whopper", "Beast", "Offside","So","Big","Cin","MAX","Error","Red","Kraga","Bangs"]
+son_grup = ["404","McDonalds","McCrappe","Werner","Serious","Trump","VERSTAPPEN","Stalin","Bottler","Bulls","Gyatt"]
 def name():
-    ilk_eleman = ilk_grup[random.randint(0,9)]
-    ikinci_eleman = ikinci_grup[random.randint(0,9)]
-    son_eleman = son_grup[random.randint(0,9)]
+    ilk_eleman = ilk_grup[random.randint(0,10)]
+    ikinci_eleman = ikinci_grup[random.randint(0,10)]
+    son_eleman = son_grup[random.randint(0,10)]
     print(f"{ilk_eleman} {ikinci_eleman} {son_eleman}")
 repeat = int(input("Kaç Tekrar İstersiniz? "))
 for i in range(repeat):
